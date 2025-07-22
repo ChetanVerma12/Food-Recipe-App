729e34299f447f59edfddd8bc4f00a2dbe640115
ChetanVerma12 <166905162+ChetanVerma12@users.noreply.github.com>
2025-07-22 17:30:06 +0530

    Update RecipeItems.jsx

--- frontend/food-blog-app/src/components/RecipeItems.jsx
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -24,7 +24,7 @@ function RecipeItems() {
   },[recipes])
 
   const onDelete= async(id)=>{
-     await axios.delete(`http://localhost:5000/recipe/${id}`)
+     await axios.delete(`https://food-recipe-app-back-h80p.onrender.com/recipe/${id}`)
      .then((res)=>console.log(res))
      setAllRecipes(recipes=>recipes.filter(recipe=>recipe._id !== id))
       let filterItem = favItems.filter(recipe=>recipe._id !==id)
